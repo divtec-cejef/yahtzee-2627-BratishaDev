5a3c87a11798dab4fa8bb04017cffde17db77598
Andrii <andrii.dimov@divtec.ch>
2026-09-10 15:44:50 +0200

    classe dice v0.6

--- src/Objets/DiceHand.java
+++ src/Objets/DiceHand.java
@@ -6,13 +6,14 @@ package Objets;
 public class DiceHand {
 
     private final int NUMBER_OF_DICE = 5;
-    private int[] dice = new int[NUMBER_OF_DICE];
+    private Die[] dice = new Die[NUMBER_OF_DICE];
 
-
-    int[] rollDice(){
-        for (int index = 0; index < dice.length; index++) {
-            dice[index] = roll();
+    void rollDice(){
+        for (Die die : dice) {
+            die.roll();
         }
-        return dice;
     }
+
+
+
 }
