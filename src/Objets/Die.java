5ca1b38fe3479d9d2e4c62b2a2c3eaff9139adfd
Andrii <andrii.dimov@divtec.ch>
2026-09-10 13:27:41 +0200

    classe die v1

--- src/Objets/Die.java
+++ src/Objets/Die.java
@@ -1,4 +1,45 @@
 package Objets;
 
+/**
+ * Classe qui représente un dé à jouer.
+ */
 public class Die {
+
+    private final int SIDE_COUNT = 6;
+    private int visibleSide = 1;
+
+    /**
+     * Constructeur par défaut.
+     */
+    Die() {
+
+    }
+
+    /**
+     * Lance le dé.
+     * @return le résultat obtenu.
+     */
+    public int roll() {
+        visibleSide = (int)(Math.random() * SIDE_COUNT + 1);
+        return visibleSide;
+    }
+
+    /**
+     * @return la face visible exposée par ce dé.
+     */
+    public int getVisibleSide() {
+        return visibleSide;
+    }
+
+    /**
+     * Modifie la face visible de ce dé.
+     * @param visibleSide Nouvelle face visible.
+     */
+    public void setVisibleSide(int visibleSide) {
+        if (visibleSide >= 1 && visibleSide <= SIDE_COUNT) {
+            this.visibleSide = visibleSide;
+        }
+    }
+
+
 }
