33cda1ce288fbe213e7415e7ed24064fab277a19
Andrii <andrii.dimov@divtec.ch>
2026-09-23 15:30:24 +0200

    feat: scorecard 80%

--- src/Objets/Scorecard.java
+++ src/Objets/Scorecard.java
@@ -1,4 +1,23 @@
 package Objets;
+import java.util.Map;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Scorecard {
+
+    private Map<Category, ScoreEntry> entries = new EnumMap<>(Category.class);
+
+    public void saveScore(Category category, DiceHand diceHand){
+        ScoreEntry entry = new ScoreEntry(category,diceHand);
+        entries.put(category, entry);
+    }
+
+    public int getTotalScore(){
+        int totalScore = 0;
+        for (ScoreEntry entry : entries.values()){
+            totalScore += entry.getScore();
+        }
+        return totalScore;
+    }
 }
