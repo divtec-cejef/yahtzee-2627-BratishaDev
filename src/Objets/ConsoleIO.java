1c8c5954c8b039e02eb76df682838717dfe0eeb6
Andrii <andrii.dimov@divtec.ch>
2026-09-24 15:28:49 +0200

    feat: Classe ConsoleIO méthode occurrencesDisplay

--- src/Objets/ConsoleIO.java
+++ src/Objets/ConsoleIO.java
@@ -2,9 +2,24 @@ package Objets;
 
 public class ConsoleIO {
 
+    /**
+     * Affiche les dés et les chiffres qui sont sortis
+     * @param diceHand dés à afficher
+     */
     public void displayDice(int[] diceHand) {
         for (int index = 0; index < diceHand.length; index++) {
             System.out.println("Des numero " + (index + 1) + ": " + diceHand[index]);
+        }
+        System.out.println();
+    }
+
+    /**
+     * Affiche des occurrences
+     * @param occurrances occurrences à afficher
+     */
+    public void occurrencesDisplay(int[] occurrances) {
+        for (int index = 0; index < occurrances.length; index++) {
+            System.out.println("Occurrences de la face " + (index + 1) + ": " + occurrances[index]);
         }
         System.out.println();
     }
